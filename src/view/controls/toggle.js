84ebc0ad74557f8817844bca24f017111035504d
cold6lanket <kz9merek@gmail.com>
2023-08-24 06:13:55 +0000

    toggle v2

--- src/view/controls/toggle.js
+++ src/view/controls/toggle.js
@@ -3,55 +3,63 @@ import {Control} from "./control";
 import {types} from "../../types";
 import styles from "./toggle.css";
 
-export class Toggle extends Control{
-
+export class Toggle extends Control {
     static options = {
-        value:{type: types.primitives.Bool, default:false},
-        textOn: {type: types.primitives.String, default:"on"},
-        textOff: {type: types.primitives.String, default:"off"}
+        value: { type: types.primitives.Bool, default: false },
+        textOn: { type: types.primitives.String, default: "on" },
+        textOff: { type: types.primitives.String, default: "off" },
     };
 
-    static markup = `<label class="${ styles.switch }">
-        <input name="switch-input" class="${ styles.toggle_input }" type="checkbox">
-        <span class="${ styles.slider } ${ styles.round }"></span>
-        <span name="switch-on" class="${styles["switch_text-on"]}"></span>
-        <span name="switch-off" class="${styles["switch_text-off"]}"></span>
+    static markup = `<label class="${styles["toggle"]} ${styles["toggle-lg"]}">
+        <input name="toggle-input" type="checkbox" class="${styles["toggle-input"]}" aria-label="Close" aria-checked="false" role="switch">
+        <span class="${styles["toggle-presentation"]}">
+            <span name="inner-text" class="${styles["toggle-inner"]}"></span>
+        </span>
     </label>`;
 
-    constructor( options ){
-        super( options );
+    constructor(options) {
+        super(options);
 
-        this.$switch = this.$markup.find('[name="switch-input"]');
-        const $on = this.$markup.find('[name="switch-on"]');
-        const $off = this.$markup.find('[name="switch-off"]');
+        this.$switch = this.$markup.find('[name="toggle-input"]');
 
-        this.bind("textOn", val=> $on.text( val ) );
-        this.bind("textOff", val=> $off.text( val ) );
+        const $innerText = this.$markup.find('[name="inner-text"]');
 
-        ["mousedown","mouseup","click"].forEach(event => this.$switch.on(event, e=>{
-            e.stopPropagation();
-        }));
-
-        this.$switch.on("change", e=>{
-            e.preventDefault();
-            e.stopPropagation();
-            this.set({ value: this.$switch.prop('checked')});
+        this.bind("textOn", (val) => {
+            if (this.get("value")) { $innerText.text(val); }
         });
 
-        this.bind("value", val => {
-            this.$markup.toggleClass(styles.on, val);
-        })
+        this.bind("textOff", (val) => {
+            if (!this.get("value")) { $innerText.text(val); }
+        });
+
+        ["mousedown", "mouseup", "click"].forEach((event) =>
+            this.$switch.on(event, (e) => { e.stopPropagation(); })
+        );
+
+        this.$switch.on("change", (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            this.set({ value: this.$switch.prop("checked") });
+        });
+
+        this.bind("value", (val) => {
+            this.$markup.toggleClass(styles["toggle-checked"], val);
+            this.$switch.attr("aria-label", val ? "Open" : "Close");
+            this.$switch.attr("aria-checked", val ? "true" : "false");
+            
+            $innerText.text(val ? this._options.textOn : this._options.textOff);
+        });
     }
 
-    updateValue( value=false, prev ){
-        this.$switch.prop('checked', value);
+    updateValue(value = false, prev) {
+        this.$switch.prop("checked", value);
     }
 
-    enable( value ){
-        this.$switch.prop('disabled', !value);
+    enable(value) {
+        this.$switch.prop("disabled", !value);
     }
 
-    focus(){
+    focus() {
         this.$switch.focus();
     }
 }
