328fba04e4399aaee278edb004e54f5f4ae83dc7
roman <vzroman@gmail.com>
2024-02-22 22:11:27 +0600

    lock view items while waiting for data

--- src/utilities/waiting.js
+++ src/utilities/waiting.js
@@ -1,6 +1,22 @@
 
 export function waiting( $container ){
-    $container = $container ?? $('body');
-    const $lock=$('<div class="overlay"><div class="wait-img"></div></div>').appendTo($container);
+    const $body = $('body');
+    const $lock=$('<div class="overlay"><div class="wait-img"></div></div>').appendTo($body);
+    let top, left, width, height;
+    if ($container){
+        const offset = $container.offset();
+        top = offset.top;
+        left = offset.left;
+        width = $container.outerWidth();
+        height = $container.outerHeight();
+    }else{
+        top = 0;
+        left = 0;
+        width = $container.outerWidth();
+        height = $container.outerHeight();
+    }
+
+    $lock.css({top, left, width:`${width}px`, height:`${height}px`});
+
     return function () { $lock.remove(); }
 }
