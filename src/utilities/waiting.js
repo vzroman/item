81872898b60f2bfb8338f28db4396a0bdd194669
roman <vzroman@gmail.com>
2024-02-22 22:24:17 +0600

    lock view items while waiting for data

--- src/utilities/waiting.js
+++ src/utilities/waiting.js
@@ -1,22 +1,21 @@
 
 export function waiting( $container ){
     const $body = $('body');
+    const $element = $container ?? $body;
     const $lock=$('<div class="overlay"><div class="wait-img"></div></div>').appendTo($body);
-    let top, left, width, height;
-    if ($container){
-        const offset = $container.offset();
-        top = offset.top;
-        left = offset.left;
-        width = $container.outerWidth();
-        height = $container.outerHeight();
-    }else{
-        top = 0;
-        left = 0;
-        width = $container.outerWidth();
-        height = $container.outerHeight();
+    const onResize = ()=>{
+        const {top, left} = $container.offset();
+        const width = $container.outerWidth();
+        const height = $container.outerHeight();
+        $lock.css({top, left, width:`${width}px`, height:`${height}px`});
     }
+    const resizeObserver = new ResizeObserver(() => {
+        onResize()
+    });
+    resizeObserver.observe($element["0"]);
 
-    $lock.css({top, left, width:`${width}px`, height:`${height}px`});
-
-    return function () { $lock.remove(); }
+    return function () {
+        resizeObserver.disconnect();
+        $lock.remove();
+    }
 }
