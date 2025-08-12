9191c5f83b26ee7927cd7f2f0498ec77f78f379d
Dalen0 <hoxsto.versel@gmail.com>
2025-08-12 16:21:17 +0500

    - destroy notify dialog after end of timeout

--- src/dialogs/notify.js
+++ src/dialogs/notify.js
@@ -2,16 +2,28 @@ import {item} from "../index.js"
 import icon from "./notify.svg"
 import styles from "./notify.css"
 
-export function notify( text ){
-    return new Promise(resolve =>{
-        new item.view.layout.Window({
-            icon:`url("${ icon }")`,
-            actions:["close"],
-            view:{
-                view:item.view.primitives.Label,
-                options:{ text, classes:[styles.notify]}
+export function notify(text, timeout) {
+    return new Promise(resolve => {
+        let dialog = new item.view.layout.Window({
+            icon: `url("${icon}")`,
+            actions: ["close"],
+            view: {
+                view: item.view.primitives.Label,
+                options: { text, classes: [styles.notify] }
             },
-            events:{ destroy:resolve }
+            events: {
+                destroy: () => {
+                    clearTimeout(timer);
+                    resolve();
+                }
+            }
         });
-    })
+
+        let timer;
+        if (timeout && typeof timeout === 'number') {
+            timer = setTimeout(() => {
+                dialog.destroy();
+            }, timeout);
+        }
+    });
 }
