a23a0e8d754b03c41c446e589c593df95f2ad8e8
Damir Tagankhozhaev <damir.tagankhozhaev@faceplate.io>
2026-04-08 14:34:27 +0500

    fix:fixed by saving default orderby and using it on reset

--- src/view/collections/treeGrid.js
+++ src/view/collections/treeGrid.js
@@ -87,6 +87,9 @@ export class TreeGrid extends ItemView{
             }
         };
 
+        //--------Save default orderBy to restore on search reset----------
+        this._defaultOrderBy = this._options.data?.option("orderBy");
+
         //-------------Proxy grid events--------------------------
         this._gridOptions.events = Object.fromEntries(["onSelect","rowDblClick"].map(e => {
             return [e, (...args)=>this._trigger(e,args) ]
@@ -244,8 +247,7 @@ export class TreeGrid extends ItemView{
         this._widgets.search_bar.set({"visible": false, value:""});
         this._widgets.search_icon.set({"visible": true});
         if (!(this._grid.get("columns")[0].view instanceof TreeCell)) {
-            const orderBy = this._grid?.getContext()?.get("$.orderBy");
-            this._options.data?.option("orderBy", orderBy?.[0]);
+            this._options.data?.option("orderBy", this._defaultOrderBy);
             this._contextPath( this._options.contextPath );
         }
     }
